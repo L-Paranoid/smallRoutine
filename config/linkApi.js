f8f7b050221ccb0d9f4f93537640372f4c01f690
梁宇豪 <yuhao.liang@yunshipei.com>
2020-01-02 22:52:24 +0800

    添加分类商品页面

--- config/linkApi.js
+++ config/linkApi.js
@@ -3,5 +3,4 @@ module.exports ={
   AuthLoginByWeixin: ApiRootUrl + 'auth/loginByWeixin', //微信登陆地址
   IndexUrl:ApiRootUrl+'index/index', //首页全部数据接口地址
   CatalogCurrent: ApiRootUrl + 'catalog/current', //分类当前分类数据
-
 }
